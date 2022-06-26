2bfd8251a1e4a6c4798940309773a33aad22dfa0
Amine Ilidrissi <amine.ilidrissi@banana-877.com>
2022-06-26 23:35:22 +0900

    Add Tailwind

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="w-screen h-screen flex items-center justify-center">
+    <h1 class="text-6xl font-bold">It works! 🎉</h1>
   </div>
 </template>
